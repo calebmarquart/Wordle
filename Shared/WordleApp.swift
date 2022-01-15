4063878a06be6c7602e3fc26cf2bf1f82186f80a
calebmarquart <marquartcaleb@gmail.com>
2022-01-14 18:19:05 -0700

    Initial Commit

---
+++ Shared/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Shared
+//
+//  Created by Caleb Marquart on 2022-01-14.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
